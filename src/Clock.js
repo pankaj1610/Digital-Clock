0508dc27c295e38825bf5a33b159493a447ff166
pankaj1610 <pankajsha584@gmail.com>
2023-07-09 23:44:07 +0530

    cleaned boilerplate react code, setup and build basic clock component

---
+++ src/Clock.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const getCurrentTime = () => {
+	const curerntTime = new Date();
+	const hours = curerntTime.getHours();
+	const minutes = curerntTime.getMinutes();
+	const seconds = curerntTime.getSeconds();
+	return [hours, minutes, seconds];
+};
+
+const Clock = () => {
+	const [hours, setHours] = useState();
+	const [minutes, setMinutes] = useState();
+	const [seconds, setSeconds] = useState();
+
+	setInterval(() => {
+		const [hours, minutes, seconds] = getCurrentTime();
+		setHours(hours);
+		setMinutes(minutes);
+		setSeconds(seconds);
+	}, 1000);
+
+	return (
+        <div className="clock-container">  
+			<div className="hrs-container clock-com">{hours}:</div>
+			<div className="min-container clock-com">{minutes}:</div>
+			<div className="sec-container clock-com">{seconds}</div>
+		</div>
+	);
+};
+
+export default Clock;
